299325ac2039d2397a1b2f93b792834d73ae2ae1
Ajay singh <79908532+Ajaysingh647@users.noreply.github.com>
2021-03-19 10:37:33 +0530

    prime_or_not

---
+++ program_1.py
@@ -0,0 +1,11 @@
+#programcheck the prime or not
+c=1
+n=int(input('Enter the number'))
+for i in range(1,n+1):
+    if(x%1==0):
+        c=c+1
+    if(c==2):
+        print('No. is prime')
+    else:
+        print('No. is not prime')
+    
